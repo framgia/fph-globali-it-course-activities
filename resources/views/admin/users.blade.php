eb5f52fc5e2273b04381f2bcf258c42fbc98cd04
Christan Shane D. Plaza <37991446+christanplaza@users.noreply.github.com>
2020-05-27 18:05:30 +0800

    [feature/admin-dashboard] Admin Dashboard (Categories/Users) (#18)

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Admin Dashboard | Users</h1>
+        <p>
+          <a href="{{ route('admin.categories') }}" class="mr-4">Categories</a>
+          <a href="{{ route('admin.users') }}">Users</a>
+        </p>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table">
+                    <thead class="thead-dark">
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                      </tr>
+                    </tbody>
+                  </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function() {
+    Route::get('/dashboard/categories', 'AdminController@categories')->name('admin.categories');
+    Route::get('/dashboard/users', 'AdminController@users')->name('admin.users');
+});
